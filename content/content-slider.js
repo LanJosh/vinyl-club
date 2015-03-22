87c29e59a15e46c77b5c7154e103c4d22336ee21
Joshua Lan <lan.74@buckeyemail.osu.edu>
2015-03-22 14:40:34 -0400

    content slidera and containers added

--- content/content-slider.js
+++ content/content-slider.js
@@ -2,35 +2,55 @@
 
 $(function () {
 	$("#about").click(function() {
-		$("#main-logo").hide();
+		$("#main-logo").fadeOut(1000);
+		if ($(".active").length !== 0) {
 		$(".active").animate({width: "0"}, 2000) 
 		$(".active").removeClass("active");
+		};
+		$("#about-content").animate({width: "75%"}, 2000);
+		$("#about-content").animate({borderWidth: "1"}, 'fast', function() {
 		$("#about-page").animate({width: "50%"}, 2000);
 		$("#about-page").addClass("active");
+		});
 	});
 	
 	$("#playlist").click(function() {
-		$("#main-logo").hide();
-		$("div.active").animate({width: "0"}, 2000)
-		$("div.active").removeClass("active");
-		$("#playlist-page").animate({width: "50%"}, 2000);
-		$("#playlist-page").addClass("active");
+		$("#main-logo").fadeOut(1000);
+		if ($(".active").length !== 0) {
+		$(".active").animate({width: "0"}, 2000) 
+		$(".active").removeClass("active");
+		};
+		$("#about-content").animate({width: "75%"}, 2000);
+		$("#about-content").animate({borderWidth: "1"}, 'fast', function() {
+		$("#about-page").animate({width: "50%"}, 2000);
+		$("#about-page").addClass("active");
+		});
 	});
 	
 	$("#membership").click(function() {
-		$("#main-logo").hide();
-		$("div.active").animate({width: "0"}, 2000) 
-		$("div.active").removeClass("active");
-		$("#membership-page").animate({width: "50%"}, 2000);
-		$("#membership-page").addClass("active");
+		$("#main-logo").fadeOut(1000);
+		if ($(".active").length !== 0) {
+		$(".active").animate({width: "0"}, 2000) 
+		$(".active").removeClass("active");
+		};
+		$("#about-content").animate({width: "75%"}, 2000);
+		$("#about-content").animate({borderWidth: "1"}, 'fast', function() {
+		$("#about-page").animate({width: "50%"}, 2000);
+		$("#about-page").addClass("active");
+		});
 	});
 
 	$("#tracks").click(function() {
-		$("#main-logo").hide();
-		$("div.active").animate({width: "0"}, 2000)
-		$("div.active").removeClass("active");
-		$("#tracks-page").animate({width: "50%"}, 2000);
-		$("#tracks-page").addClass("active");
+		$("#main-logo").fadeOut(1000);
+		if ($(".active").length !== 0) {
+		$(".active").animate({width: "0"}, 2000) 
+		$(".active").removeClass("active");
+		};
+		$("#about-content").animate({width: "75%"}, 2000);
+		$("#about-content").animate({borderWidth: "1"}, 'fast', function() {
+		$("#about-page").animate({width: "50%"}, 2000);
+		$("#about-page").addClass("active");
+		});
 	});
 });
 

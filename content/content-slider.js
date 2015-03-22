2717ba5fb08ab924e825e84514210c793f63fe26
Joshua Lan <lan.74@buckeyemail.osu.edu>
2015-03-22 01:35:51 -0400

    animation update

--- content/content-slider.js
+++ content/content-slider.js
@@ -1,41 +1,36 @@
+// TODO flesh out helper function
+
 $(function () {
 	$("#about").click(function() {
 		$("#main-logo").hide();
+		$(".active").animate({width: "0"}, 2000) 
+		$(".active").removeClass("active");
 		$("#about-page").animate({width: "50%"}, 2000);
-		$("#about-page").animate({width: "0%"}, 2000);
+		$("#about-page").addClass("active");
 	});
+	
 	$("#playlist").click(function() {
 		$("#main-logo").hide();
+		$("div.active").animate({width: "0"}, 2000)
+		$("div.active").removeClass("active");
 		$("#playlist-page").animate({width: "50%"}, 2000);
-		$("#playlist-page").animate({width: "0%"}, 2000);
+		$("#playlist-page").addClass("active");
 	});
+	
 	$("#membership").click(function() {
 		$("#main-logo").hide();
+		$("div.active").animate({width: "0"}, 2000) 
+		$("div.active").removeClass("active");
 		$("#membership-page").animate({width: "50%"}, 2000);
-		$("#membership-page").animate({width: "0%"}, 2000);
+		$("#membership-page").addClass("active");
 	});
+
 	$("#tracks").click(function() {
 		$("#main-logo").hide();
+		$("div.active").animate({width: "0"}, 2000)
+		$("div.active").removeClass("active");
 		$("#tracks-page").animate({width: "50%"}, 2000);
-		$("#tracks-page").animate({width: "0%"}, 2000);
+		$("#tracks-page").addClass("active");
 	});
-/*	$("#about").click(slideContent("#about"));
-	$("#playlist").click(slideContent("#playlist"));
-	$("#membership").click(slideContent("#membership"));
-	$("#tracks").click(slideContent("#tracks")); */
 });
-/*
-function slideContent(content) {
-	var contentToActive		= $(content);
-	var contentActive			= $(".active");
-
-	if (contentActive.length != 0) {
-		contentActive.toggle(1000);
-		contentActive.removeClass("active");
-	};
-	contentToActive.addClass("active");
-	contentToActive.toggle();
-	contentToActive.animate({right: "50%", left: "0"}, 3000);	
-}*/
-
 

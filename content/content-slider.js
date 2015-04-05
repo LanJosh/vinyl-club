a95341498530146a5dd2e744f44bd4d343035f36
Joshua Lan <lan.74@buckeyemail.osu.edu>
2015-04-05 14:23:53 -0400

    Changed animation logic

--- content/content-slider.js
+++ content/content-slider.js
@@ -4,27 +4,23 @@ $(function () {
 	$("#about").click(function() {
 		$("#main-logo").fadeOut(1000);
 		if ($(".active").length !== 0) {
-		$(".active").animate({width: "0"}, 2000) 
+		$(".active").animate({right: "0", left: "50%" }, 2000) 
 		$(".active").removeClass("active");
 		};
-		$("#about-content").animate({width: "75%"}, 2000);
-		$("#about-content").animate({borderWidth: "1"}, 'fast', function() {
-		$("#about-page").animate({width: "50%"}, 2000);
+		$("#about-page").fadeIn();
+		$("#about-page").animate({left: "0%", right: "50"}, 2000);
 		$("#about-page").addClass("active");
-		});
 	});
 	
 	$("#playlist").click(function() {
 		$("#main-logo").fadeOut(1000);
 		if ($(".active").length !== 0) {
-		$(".active").animate({width: "0"}, 2000) 
+		$(".active").animate({right: "0", left: "50%"}, 2000) 
 		$(".active").removeClass("active");
 		};
-		$("#about-content").animate({width: "75%"}, 2000);
-		$("#about-content").animate({borderWidth: "1"}, 'fast', function() {
-		$("#about-page").animate({width: "50%"}, 2000);
-		$("#about-page").addClass("active");
-		});
+		$("#playlist-page").fadeIn();	
+		$("#playlist-page").animate({left: "0%", right: "50%"}, 2000);
+		$("#playlist-page").addClass("active");
 	});
 	
 	$("#membership").click(function() {
@@ -33,11 +29,9 @@ $(function () {
 		$(".active").animate({width: "0"}, 2000) 
 		$(".active").removeClass("active");
 		};
-		$("#about-content").animate({width: "75%"}, 2000);
-		$("#about-content").animate({borderWidth: "1"}, 'fast', function() {
-		$("#about-page").animate({width: "50%"}, 2000);
-		$("#about-page").addClass("active");
-		});
+		$("membership-page").fadeIn();
+		$("#membership-page").animate({left: "0%", right: "50%"}, 2000);
+		$("#membership-page").addClass("active");
 	});
 
 	$("#tracks").click(function() {
@@ -46,11 +40,9 @@ $(function () {
 		$(".active").animate({width: "0"}, 2000) 
 		$(".active").removeClass("active");
 		};
-		$("#about-content").animate({width: "75%"}, 2000);
-		$("#about-content").animate({borderWidth: "1"}, 'fast', function() {
-		$("#about-page").animate({width: "50%"}, 2000);
+		$("tracks-page").fadeIn();
+		$("#about-page").animate({left: "0%", right: "50%"}, 2000);
 		$("#about-page").addClass("active");
-		});
 	});
 });
 

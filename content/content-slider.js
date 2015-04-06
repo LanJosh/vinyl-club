ae0d3121355511d8dda0fd936d3a3fedc3e18b8d
Joshua Lan <lan.74@buckeyemail.osu.edu>
2015-04-05 21:06:21 -0400

    Updates to animations, content added

--- content/content-slider.js
+++ content/content-slider.js
@@ -4,45 +4,45 @@ $(function () {
 	$("#about").click(function() {
 		$("#main-logo").fadeOut(1000);
 		if ($(".active").length !== 0) {
-		$(".active").animate({right: "0", left: "50%" }, 2000) 
+		$(".active").animate({right: "0", left: "50%" }, 1500) 
 		$(".active").removeClass("active");
 		};
 		$("#about-page").fadeIn();
-		$("#about-page").animate({left: "0%", right: "50"}, 2000);
+		$("#about-page").animate({left: "0%", right: "50"}, 1500);
 		$("#about-page").addClass("active");
 	});
 	
 	$("#playlist").click(function() {
 		$("#main-logo").fadeOut(1000);
 		if ($(".active").length !== 0) {
-		$(".active").animate({right: "0", left: "50%"}, 2000) 
+		$(".active").animate({right: "0", left: "50%"}, 1500) 
 		$(".active").removeClass("active");
 		};
 		$("#playlist-page").fadeIn();	
-		$("#playlist-page").animate({left: "0%", right: "50%"}, 2000);
+		$("#playlist-page").animate({left: "0%", right: "50%"}, 1500);
 		$("#playlist-page").addClass("active");
 	});
 	
 	$("#membership").click(function() {
 		$("#main-logo").fadeOut(1000);
 		if ($(".active").length !== 0) {
-		$(".active").animate({width: "0"}, 2000) 
+		$(".active").animate({right: "0", left: "50%"}, 1500) 
 		$(".active").removeClass("active");
 		};
-		$("membership-page").fadeIn();
-		$("#membership-page").animate({left: "0%", right: "50%"}, 2000);
+		$("#membership-page").fadeIn();
+		$("#membership-page").animate({left: "0%", right: "50%"}, 1500);
 		$("#membership-page").addClass("active");
 	});
 
 	$("#tracks").click(function() {
 		$("#main-logo").fadeOut(1000);
 		if ($(".active").length !== 0) {
-		$(".active").animate({width: "0"}, 2000) 
+		$(".active").animate({right: "0", left: "50%"}, 1500) 
 		$(".active").removeClass("active");
 		};
-		$("tracks-page").fadeIn();
-		$("#about-page").animate({left: "0%", right: "50%"}, 2000);
-		$("#about-page").addClass("active");
+		$("#tracks-page").fadeIn();
+		$("#tracks-page").animate({left: "0%", right: "50%"}, 1500);
+		$("#tracks-page").addClass("active");
 	});
 });
 
